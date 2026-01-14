27fc3d75680425b2a03a9b39a493fab69a161d60
aydin <aydin4ik@gmail.com>
2026-01-14 22:01:48 +1100

    Fixed cache folders issue and vite not building

--- startup.sh
+++ startup.sh
@@ -2,7 +2,12 @@ echo "Running startup.sh"
 cp /home/site/wwwroot/nginx-default /etc/nginx/sites-available/default && service nginx reload && service supervisor restart
 
 # Copy .user.ini to public directory
-cp /home/site/wwwroot/.user.ini /home/site/wwwroot/public/.user.ini
+cp /home/site/wwwroot/user.ini /home/site/wwwroot/public/.user.ini
 
 php /home/site/wwwroot/artisan migrate --force
 echo "Migrated database"
+
+# Create cache folders
+cd storage/
+mkdir -p framework/{sessions,views,cache}
+chmod -R 775 framework
